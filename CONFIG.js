333d85d9a094430c8b83d557e99f683623c8eeee
Vlada Janosevic <vladajanosevic@gmail.com>
2019-04-22 00:34:44 +0200

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,8 +1,5 @@
 const CONFIG = {
-	apiKey: "AIzaSyBKMQz3D-8QJgrg97mBVFSQvJg2Qz9sfVM",
-	authDomain: "basic-c2571.firebaseapp.com",
-	databaseURL: "https://basic-c2571.firebaseio.com",
-	projectId: "basic-c2571",
-	storageBucket: "basic-c2571.appspot.com",
-	messagingSenderId: "108161882979"
+	apiKey: '### FIREBASE API KEY ###',
+	authDomain: '### FIREBASE AUTH DOMAIN ###',
+	projectId: '### CLOUD FIRESTORE PROJECT ID ###'
 };
