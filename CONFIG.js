7478566afa3fa43a2639d058f351e0d530203544
vlada-j <vladajanosevic@gmail.com>
2019-04-22 00:23:10 +0200

    Logged and not logged posts

---
+++ CONFIG.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+	apiKey: "AIzaSyBKMQz3D-8QJgrg97mBVFSQvJg2Qz9sfVM",
+	authDomain: "basic-c2571.firebaseapp.com",
+	databaseURL: "https://basic-c2571.firebaseio.com",
+	projectId: "basic-c2571",
+	storageBucket: "basic-c2571.appspot.com",
+	messagingSenderId: "108161882979"
+};
